051c8e607a0c83a513aa65e10fe09c31deada5d1
ClockFox <44942917+FoxClock@users.noreply.github.com>
2026-02-22 21:33:33 +1100

    Added cSettings flags directing CLANG to look for import directories in both X86 and ARM64 locations.
    Added all build targets to test target as well, so the program compiles under test and build instructions.

--- Sources/CFreeTDS/include/CFreeTDS.h
+++ Sources/CFreeTDS/include/CFreeTDS.h
@@ -3,17 +3,7 @@
 
 #pragma once
 
-#ifdef __APPLE__
-    #ifdef __x86_64__
-    #include "/usr/local/opt/freetds/include/sybdb.h"
-    #include "/usr/local/opt/freetds/include/sybfront.h"
-    #else
-    #include "/opt/homebrew/opt/freetds/include/sybdb.h"
-    #include "/opt/homebrew/opt/freetds/include/sybfront.h"
-    #endif
-#else
-    #include <sybdb.h>
-    #include <sybfront.h>
-#endif
+#include <sybdb.h>
+#include <sybfront.h>
 
 #endif /* CFREETDS_H */
